5e778c44ff43a7e19e66e4db285770252bfc03f7
girl-physicist <girl-physicist@mail.ru>
2017-05-14 13:14:42 +0300

    Change Task 2 Part 5

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -18,39 +18,26 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-           
             string line = "=============================================================";
             string line2 = String.Empty;
             string fileName = "input.txt";
             IReader r = new Reader(fileName);
+            ISubjectIndex cl = new SubjectIndex(fileName);
+            IParser<Text> parser = new TextParser();
+            IDictionary<char, string[]> word = cl.Some(2);
             IEnumerable<string> listSentences = new List<string>();
             IEnumerable<string> listSentences1 = new List<string>();
-            IEnumerable<string> listSentences3 = new List<string>();
-            IParser<Text> parser = new TextParser();
-
-            Class1 cl = new Class1();
-           var vvv= cl.Some(2, fileName);
-
             listSentences = r.Read(TypeOfRead.OriginalText);
             listSentences1 = r.Read(TypeOfRead.SpliText);
-            listSentences3 = r.Read(TypeOfRead.TexstToLower);
             var text = parser.Parse(listSentences1);
-
-            string word = vvv.Item1;
-            int countEntries = vvv.Item2;
-            string pages = vvv.Item3;
-
-            Console.WriteLine("{0}....Count: {1},Pages: {2}", word
-                , countEntries, string.Join(",", pages.Select(item => item.ToString())));
-
             // 0 Вывод исходной версии
-            Console.WriteLine("Original version");
+            Console.WriteLine("=====Original version=====");
             Console.WriteLine(line2);
             Console.WriteLine(parser.Parse(listSentences));
             Console.WriteLine(line);
             Console.WriteLine();
             // 1 Вывести все предложения заданного текста в порядке возрастания количества слов в каждом из них.
-            Console.WriteLine("1 Output all sentences of the given text in ascending order of the number of words in each of them.");
+            Console.WriteLine("=====1 Output all sentences of the given text in ascending order of the number of words in each of them.=====");
             Console.WriteLine(line2);
             foreach (var item in text.SortSentencesByWordsCount())
             {
@@ -58,7 +45,7 @@ namespace Task_2
             }
             Console.WriteLine(line);
             // 2 Во всех вопросительных предложениях текста найти и напечатать без повторений слова заданной длины.
-            Console.WriteLine("2 In all interrogative sentences of the text, find and print without repetition words of a given length.");
+            Console.WriteLine("=====2 In all interrogative sentences of the text, find and print without repetition words of a given length.=====");
             Console.WriteLine(line2);
             Console.WriteLine("Enter the length of the word");
             var length = Convert.ToInt32(Console.ReadLine());
@@ -69,7 +56,7 @@ namespace Task_2
             }
             Console.WriteLine(line);
             // 3 Из текста удалить все слова заданной длины, начинающиеся на согласную букву.
-            Console.WriteLine("3 Delete all words of a given length, starting with a consonant letter from the text.");
+            Console.WriteLine("=====3 Delete all words of a given length, starting with a consonant letter from the text.=====");
             Console.WriteLine(line2);
             Console.WriteLine("Enter the length of the word");
             var length2 = Convert.ToInt32(Console.ReadLine());
@@ -78,8 +65,8 @@ namespace Task_2
             Console.WriteLine(line);
             // 4. В некотором предложении текста слова заданной длины заменить указанной подстрокой, 
             // длина которой может не совпадать с длиной слова.
-            Console.WriteLine("4 In some sentence of the text of a word of a given length, " +
-                              "replace the specified substring whose length may not coincide with the length of the word.");
+            Console.WriteLine("=====4 In some sentence of the text of a word of a given length, " +
+                              "replace the specified substring whose length may not coincide with the length of the word.=====");
             Console.WriteLine(line2);
             Console.WriteLine("Enter the sentence number");
             var sentence = Convert.ToInt32(Console.ReadLine());
@@ -91,10 +78,18 @@ namespace Task_2
             Console.WriteLine(line2);
             Console.WriteLine(text);
             Console.WriteLine(line);
-
-
-           
-           
+            // Часть 2. Предметный указатель
+            Console.WriteLine(line2);
+            Console.WriteLine("=====Dictionary=====");
+            Console.WriteLine(line);
+            foreach (var ch in word)
+            {
+                Console.WriteLine(ch.Key);
+                foreach (var item in ch.Value)
+                {
+                    Console.WriteLine(item);
+                }
+            }
             Console.ReadKey();
         }
     }
